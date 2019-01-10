df3e41d3ea7c54fafddbbf41f482d6095349aed4
Albert_Lee <3185017028@qq.com>
2019-01-10 19:26:15 +0800

    第二次提交

--- TestProject/src/com/albert/types/TypeInference.java
+++ TestProject/src/com/albert/types/TypeInference.java
@@ -5,5 +5,8 @@ public class TypeInference {
 	public static void main(String[] args) {
 		final Value<String> value = new Value<>();
 		value.getOrDefault("22", Value.defaultValue());
+		
+		
+		
 	}
 }
