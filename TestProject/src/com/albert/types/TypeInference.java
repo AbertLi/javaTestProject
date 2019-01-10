2db37d05b4ac2751483b77e9ed07dd6403fe3f1e
Albert_Lee <3185017028@qq.com>
2019-01-10 19:15:16 +0800

    用于学习的Java项目

---
+++ TestProject/src/com/albert/types/TypeInference.java
@@ -0,0 +1,10 @@
+package com.albert.types;
+
+//更好的类型推测机制
+public class TypeInference {
+	public static void main(String[] args) {
+		final Value<String> value = new Value<>();
+		value.getOrDefault("22", Value.defaultValue());
+		
+	}
+}
